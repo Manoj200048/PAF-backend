a79ab09cc7b66c8acfc44ca6ed33b3fbdfea28e8
manoj <maojkavinda048@gmail.com>
2025-05-10 15:00:05 +0530

    notification

--- social-app/src/main/java/codingtechniques/model/NotificationType.java
+++ social-app/src/main/java/codingtechniques/model/NotificationType.java
@@ -3,5 +3,8 @@ package codingtechniques.model;
 public enum NotificationType {
     POST_LIKE,
     POST_COMMENT,
-    COMMENT_REPLY
+    COMMENT_REPLY,
+    COMMUNITY_JOIN,
+    COMMUNITY_INVITE,
+    COMMUNITY_POST
 }
